f17f24c5a41b47f55f9cd0648a72bb1022ae8c1a
GlaceCoding <92152391+GlaceCoding@users.noreply.github.com>
2021-10-18 14:04:45 +0200

    Fix for mac
    We use a different regex for mac (it is the setup of 42's pc)

--- test/allowed_function.sh
+++ test/allowed_function.sh
@@ -15,9 +15,13 @@ cd "$(git rev-parse --show-cdup)"
 
 FOLDER_C_FILES="./ex*"
 
-find . -type f -iname *.c -path "$FOLDER_C_FILES" -execdir gcc -c -fno-builtin {} ";"
+find . -type f -iname "*.c" -path "$FOLDER_C_FILES" -execdir gcc -c -fno-builtin {} ";"
 
-used=$(find . -type f -iname *.o -path "$FOLDER_C_FILES" -execdir nm -uA {} ";" | sed 's/.\/\(.*\): *U /\1:/g' | sed 's/\.o:/.c:/')
+if [ "Darwin" == "$(uname -s)" ]; then
+	used=$(find . -type f -iname "*.o" -path "$FOLDER_C_FILES" -execdir nm -uA {} ";" | sed 's/\(.*\): \_/\1:/g' | sed 's/\.o:/.c:/')
+else
+	used=$(find . -type f -iname "*.o" -path "$FOLDER_C_FILES" -execdir nm -uA {} ";" | sed 's/.\/\(.*\): *U /\1:/g' | sed 's/\.o:/.c:/')
+fi
 
 echo "$used"
 for scope in $used; do
